c5d642980b1a65536e6f9023cd72fdf7d0db5f65
PH Laune <laskk.next@gmail.com>
2015-10-04 19:01:19 +0200

    first commit

--- index.php
+++ index.php
@@ -1,3 +1,84 @@
+<script>
+  // This is called with the results from from FB.getLoginStatus().
+  function statusChangeCallback(response) {
+    console.log('statusChangeCallback');
+    console.log(response);
+    // The response object is returned with a status field that lets the
+    // app know the current login status of the person.
+    // Full docs on the response object can be found in the documentation
+    // for FB.getLoginStatus().
+    if (response.status === 'connected') {
+      // Logged into your app and Facebook.
+      testAPI();
+    } else if (response.status === 'not_authorized') {
+      // The person is logged into Facebook, but not your app.
+      document.getElementById('status').innerHTML = 'Please log ' +
+        'into this app.';
+    } else {
+      // The person is not logged into Facebook, so we're not sure if
+      // they are logged into this app or not.
+      document.getElementById('status').innerHTML = 'Please log ' +
+        'into Facebook.';
+    }
+  }
+
+  // This function is called when someone finishes with the Login
+  // Button.  See the onlogin handler attached to it in the sample
+  // code below.
+  function checkLoginState() {
+    FB.getLoginStatus(function(response) {
+      statusChangeCallback(response);
+    });
+  }
+
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId		: $.getenv("FACEBOOK_APP_ID"),
+      xfbml		: true,
+      cookie	: true,
+      version	: 'v2.4'
+    });
+  };
+
+  // Now that we've initialized the JavaScript SDK, we call 
+  // FB.getLoginStatus().  This function gets the state of the
+  // person visiting this page and can return one of three states to
+  // the callback you provide.  They can be:
+  //
+  // 1. Logged into your app ('connected')
+  // 2. Logged into Facebook, but not your app ('not_authorized')
+  // 3. Not logged into Facebook and can't tell if they are logged into
+  //    your app or not.
+  //
+  // These three cases are handled in the callback function.
+
+  FB.getLoginStatus(function(response) {
+    statusChangeCallback(response);
+  });
+
+  };
+
+  // Load the SDK asynchronously
+  (function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = "//connect.facebook.net/en_US/sdk.js";
+    fjs.parentNode.insertBefore(js, fjs);
+  }(document, 'script', 'facebook-jssdk'));
+
+  // Here we run a very simple test of the Graph API after login is
+  // successful.  See statusChangeCallback() for when this call is made.
+  function testAPI() {
+    console.log('Welcome!  Fetching your information.... ');
+    FB.api('/me', function(response) {
+      console.log('Successful login for: ' + response.name);
+      document.getElementById('status').innerHTML =
+        'Thanks for logging in, ' + response.name + '!';
+    });
+  }
+</script>
+
 <?php
 
 require_once __DIR__ . '/vendor/autoload.php';
@@ -7,24 +88,26 @@ $fb = new Facebook\Facebook([
 	'default_graph_version' => 'v2.4',
 	]);
 
-$helper = $fb->getCanvasHelper();
+
+$helper = $fb->getJavaScriptHelper();
 echo "HELPER = ";
 var_dump($helper);
 echo "<br><br><br>";
 try {
-	$accessToken = $helper->getAccessToken();
-	echo "ACCESSTOKEN = ";
+  $accessToken = $helper->getAccessToken();
+  echo "accessToken = ";
 	var_dump($accessToken);
 	echo "<br><br><br>";
 } catch(Facebook\Exceptions\FacebookResponseException $e) {
-	// When Graph returns an error
-	echo 'Graph returned an error: ' . $e->getMessage();
-	exit;
+  // When Graph returns an error
+  echo 'Graph returned an error: ' . $e->getMessage();
+  exit;
 } catch(Facebook\Exceptions\FacebookSDKException $e) {
-	// When validation fails or other local issues
-	echo 'Facebook SDK returned an error: ' . $e->getMessage();
-	exit;
+  // When validation fails or other local issues
+  echo 'Facebook SDK returned an error: ' . $e->getMessage();
+  exit;
 }
+
 
 if (isset($accessToken)) {
 	echo "IN ISSET ACCESSTOKEN";
