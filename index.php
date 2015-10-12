ac888e0fcbb554e47a9cb604d81e6c5760c6259b
guillaumeRot <guillaume.rot@gmail.com>
2015-10-12 19:42:10 +0100

    - Ajout background - Création feuille style CSS

--- index.php
+++ index.php
@@ -1,16 +1,13 @@
 <?php
-/*
-session_start();
-echo 'session : ' . $_SESSION['name'];
-if(isset($_SESSION['name'])) {
-	echo 'no';
-	include('login.php');
-} else {
-	echo 'ok';
-	//header('Location: login.php');
-	//include('login.php');
-	//echo 'Connected in as ' . $_SESSION['name'] . '<br>';
-}*/
 require_once 'login.php';
 
 ?>
+
+<html>
+	<head>
+		<link rel="stylesheet" type="text/css" href="style.css">
+	</head>
+	<body>
+		sfgwxfgd
+	</body>
+</html>
