1a87b8c7f89345b06d787cd5c9140d854b20e245
guillaumeRot <guillaume.rot@gmail.com>
2015-10-14 21:46:44 +0100

    Page de présentation + Page choix de jeu

---
+++ choix_jeu.php
@@ -0,0 +1,22 @@
+<?php
+include 'header.php';
+?>
+<div id="content_choix_jeu">
+	<h1>Choisissez un mode de jeu</h1>
+	<div class="choix">
+		<img src="images/earth.png"/>
+		<h2>Global</h2>
+	</div>
+	<div class="choix">
+		<img src="images/link.png"/>
+		<h2>Communauté</h2>
+	</div>
+	<div class="choix">
+		<img src="images/amis.png"/>
+		<h2>Amis</h2>
+	</div>
+</div>
+
+<?php 
+include 'footer.php';
+?>
--- index.php
+++ index.php
@@ -1,13 +1,24 @@
 <?php
+
 require_once 'login.php';
 
+include 'header.php';
 ?>
 
-<html>
-	<head>
-		<link rel="stylesheet" type="text/css" href="style.css">
-	</head>
-	<body>
-		sfgwxfgd
-	</body>
-</html>
+<img id="logo" src="images/logo_euro.png"/>
+<div class="content_presentation">
+	<div class="header">
+		<span>Euro16</span>
+	</div>
+	<div class="content">
+		<h4>Lorem ipsum dolor sit amet</h4>
+		<p>Consectetur adipiscing elit. Mauris non bibendum metus, at blandit orci. Donec ut rutrum felis. Nullam a lectus ut dui ultricies aliquet id sed ipsum. Vestibulum sit amet consectetur nibh, ac porta est. Praesent imperdiet aliquam mollis. Sed tortor magna, sollicitudin et cursus quis, consectetur vitae augue. Vivamus quam ipsum, tincidunt sit amet mauris non, mattis tempor nibh. Praesent sed sapien ac nisl luctus viverra. Vivamus elementum orci eu sem lacinia faucibus. Maecenas nec justo maximus, vehicula leo ut, mollis erat. Ut tempor congue purus, eget malesuada turpis tempor et. Etiam justo tellus, faucibus sed facilisis finibus, auctor sed nulla. Nunc lacinia velit sit amet massa tincidunt, a mollis neque finibus. Curabitur nec placerat augue. Nulla luctus iaculis orci, ac commodo lacus pulvinar non. Duis bibendum scelerisque mattis.</p>
+	</div>
+	<input id="checkbox_presentation" type="checkbox" name="ne_plus_afficher"/><span>Ne plus afficher</span>
+	<div class="footer">
+		<a id="commencer" href="choix_jeu.php">Commencer !</a>
+	</div>
+</div>		
+<?php 
+include 'footer.php';
+?>
